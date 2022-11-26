2dbe572720c4be24aa3be818f71a89cba63fa780
liyan <3590578586@qq.com>
2022-11-26 22:06:07 +0800

    作业

---
+++ day8/demo/src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    {{ mag }}
+    <p v-bind:title="message">鼠标悬停</p>
+    <p v-if="flag">哈喽</p>
+    <p v-if="!flag">再见</p>
+    <button @click="add">点击</button>
+    <p v-show="check">哈哈哈</p>
+    <p v-show="!check">呵呵呵</p>
+    <button @click="adds">点击切换</button>
+    <ul>
+      <li v-for="item in list" :key="item.id">{{item.name}}</li>
+      <button @click="checks">切换</button>
+    </ul>
+    <input type="text" v-model="value">
+    {{value}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      mag: "你好",
+      message: '页面加载于' + new Date().toLocaleString(),
+      flag:true,
+      check:true,
+      list:[
+        {
+          id:'1',
+          name:'张三'
+        },
+        {
+          id:'2',
+          name:'李四'
+        },
+        {
+          id:'3',
+          name:'老刘'
+        },
+        {
+          id:'4',
+          name:'赵六'
+        }
+      ],
+      value:''
+    };
+  },
+  methods:{
+    add(){
+      this.flag=!this.flag
+    },
+    adds(){
+      this.check=!this.check
+    },
+    checks(){
+      this.list[0].name='王五'
+    }
+  }
+};
+</script>
---
+++ day8/todolist/src/views/HomeView.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="home">
+    <div class="top">
+      <div class="tops">
+           <img src="imgs.png" alt="">
+            <button class="bth">To-Do List</button>
+        </div>
+        <h4>~Today I need to~</h4>
+        <div class="inp">
+            <input class="input" type="text" placeholder="Add new todo" >
+            <button class="but input">Submit</button>
+        </div>
+        <ul class="ul">
+            <li class="list">
+               <p>
+                    <input type="checkbox" class="boxs">
+                    <span>1111</span>
+               </p>
+                <p>x</p>
+            </li>
+        </ul>
+        <div class="fool">
+            <p>1 item left</p>
+            <div class="ap">
+                <p>All</p>
+                <p>Active</p>
+                <p>Completed</p>
+            </div>
+            <p>Clear completed</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
+<style>
+    body{
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        height: 100%;
+        background-image:linear-gradient(90deg,#ffafbd,#ffc3a0);
+        
+    }
+    .top{
+        width: 440px;
+        height: 449px;
+        background-color: #f2f2f2;
+        margin: 100px auto;
+        border-radius: 20px;
+        padding: 30px 30px;
+        box-sizing: border-box;
+        position: relative;
+    }
+    .tops{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    img{
+        width: 80px;
+        height: 80px;
+        
+    }
+    .bth{
+        width: 100px;
+        height: 44px;
+        background-color: #ea7953;
+        color: #fff;
+        font-size: 17px;
+        border: none;
+        margin-left: 10px;
+        border-radius: 5% 20% 20% 5%;
+        transform: rotate(3deg);
+    }
+    h4{
+        text-align: center;
+    }
+    .inp{
+        margin-top: 10px;
+    }
+    .input{
+        width: 270px;
+        border: none;
+        border-bottom: #ea7953 2px dashed;
+        background: transparent;
+    }
+    .but{
+        width: 70px;
+        height: 35px;
+        margin-left: 10px;
+        border: 2px solid #000;
+        background-color: #fff;
+        border-radius: 5px;
+        transform: rotate(3deg);
+    }
+    ul{
+        list-style: none;
+        padding: 0;
+    }
+     .list{
+        height: 30px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+   .boxs{
+    border: 2px solid #000;
+    background-color: #f2f2f2;
+   }
+    .fool{
+        position: absolute;
+        bottom: 30px;
+        display: flex;
+        font-size: 12px;
+        width: 85%;
+        justify-content: space-between;
+    }
+.ap{
+    display: flex;
+    width: 130px;
+    justify-content: space-between;
+}
+</style>
